fe67cdc1e8a6122b1708f2bc9b88fa802482a7ec
aps120797 <aps.120797@gmail.com>
2018-05-28 07:46:26 +0530

    Added createIssues and createLabel methods

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const getAllIssues = require('./lib/bin/getAllIssues')
 const getAllPullRequests = require('./lib/bin/getAllPullRequests')
 
-const postWeeklyDigest = require('./lib/postWeeklyDigest')
+const markdownWeeklyDigest = require('./lib/markdownWeeklyDigest')
+const postCreateLabel = require('./lib/bin/postCreateLabel')
 
 module.exports = (robot) => {
   robot.log('Yay, the app was loaded!')
-  // const supportedEvents = [
-  //   'pull_request',
-  //   'issues'
-  // ]
-  robot.on('*', async context => {
+  const supportedEvents = [
+    'pull_request',
+    'issues'
+  ]
+  robot.on('installation.created', async context => {
+    const owner = await context.payload.repository.owner.login
+    const repo = await context.payload.repository.name
+    return postCreateLabel(context, {owner, repo, name: 'weekly-digest', color: '9C27B0', description: 'Issues labeled with this label are Weekly Digests created by Weekly Digest app for GitHub. Visit github.com/probot/weekly-digest to know more.'})
+  })
+  robot.on(supportedEvents, async context => {
     const owner = await context.payload.repository.owner.login
     const repo = await context.payload.repository.name
     const issues = await getAllIssues(context, {owner, repo})
     const pullRequests = await getAllPullRequests(context, {owner, repo})
     // console.log(issues)
     // console.log(pullRequests)
-    postWeeklyDigest.postWeeklyDigest(context, {
+    return markdownWeeklyDigest(context, {
+      owner,
+      repo,
       issues,
       pullRequests
     })
