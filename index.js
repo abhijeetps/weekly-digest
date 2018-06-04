6ac4f01e54d321cdcb413f72c01cf903ea9d36f2
aps120797 <aps.120797@gmail.com>
2018-06-04 18:20:49 +0530

    Implemented probot-scheduler and more

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const createScheduler = require('probot-scheduler')
+const postCreateLabel = require('./lib/bin/postCreateLabel')
 const getAllIssues = require('./lib/bin/getAllIssues')
 const getAllPullRequests = require('./lib/bin/getAllPullRequests')
 
 const markdownWeeklyDigest = require('./lib/markdownWeeklyDigest')
-const postCreateLabel = require('./lib/bin/postCreateLabel')
 
 module.exports = (robot) => {
   robot.log('Weekly Digest app is ready to generate Weekly Reports!')
-  createScheduler(robot, {interval: 60 * 60 * 1000})
-  robot.on('installation.created', async context => {
-    const { owner, repo } = context.repo()
-    return postCreateLabel(context, {owner, repo, name: 'weekly-digest', color: '9C27B0', description: 'Issues labeled with this label are Weekly Digests created by Weekly Digest app for GitHub. Visit github.com/probot/weekly-digest to know more.'})
+  createScheduler(robot, {interval: 60 * 1000})
+  robot.on('installation.created', async (context) => {
+    const [owner, repo] = await context.payload.repositories[0].full_name.split('/')
+    return postCreateLabel(context, {owner, repo, name: 'weekly-digest', color: '9C27B0'})
   })
-  robot.on('schedule.repository', async context => {
+  robot.on('schedule.repository', async (context) => {
     const { owner, repo } = context.repo()
     const issues = await getAllIssues(context, {owner, repo})
     const pullRequests = await getAllPullRequests(context, {owner, repo})
