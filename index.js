a4a1139dd3c71f4d85e06950411a4c79e58b6846
aps120797 <aps.120797@gmail.com>
2018-06-11 21:21:48 +0530

    made installation created working

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const createWeeklyDigestLabel = require('./lib/markdown/createWeeklyDigestLabel'
 const createConfigYML = require('./lib/markdown/createConfigYML')
 const weeklyDigest = require('./lib/weeklyDigest')
 const getDate = require('./lib/markdown/getDate')
-const defaultConfig = require('./lib/markdown/defaultConfig')
+// const defaultConfig = require('./lib/markdown/defaultConfig')
 
 module.exports = (app) => {
   app.log('Weekly Digest app is ready to generate Weekly Reports!')
-  createScheduler(app, {interval: 60 * 1000}) // 1 hour
   app.on('installation.created', async (context) => {
+    console.log('App installed. installation.created triggered.')
     const [owner, repo] = await context.payload.repositories[0].full_name.split('/')
+    console.log(owner + '/' + repo)
     createWeeklyDigestLabel(context, {owner, repo})
     createConfigYML(context, {owner, repo})
     const headDate = await getDate.headDate()
@@ -18,21 +19,22 @@ module.exports = (app) => {
     const config = await getConfig(context, 'weekly-digest.yml')
     weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
   })
-  app.on('schedule.repository', async (context) => {
-    console.log(`App is running as per schedule.repository`)
-    const headDate = await getDate.headDate()
-    const tailDate = await getDate.tailDate()
-    // console.log(`${headDate}`)
-    var currentDate = await getDate.getUTCFromISO(headDate)
-    var config = await getConfig(context, 'weekly-digest.yml')
-    if (config == null) {
-      config = defaultConfig
-    }
-    if (currentDate.getDay() === config.publishDay) {
-      const { owner, repo } = context.repo()
-      // console.log(`Owner: ${owner}, Repo: ${repo}, Config: ${config.publishDay}`)
-      // console.log(`Head Date: ${headDate}, Tail Date: ${tailDate}, Current Day: ${currentDate.getDay()}`)
-      weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
-    }
-  })
+  createScheduler(app, {interval: 60 * 60 * 1000}) // 1 hour
+  // app.on('schedule.repository', async (context) => {
+  //   console.log(`App is running as per schedule.repository`)
+  //   const headDate = await getDate.headDate()
+  //   const tailDate = await getDate.tailDate()
+  //   // console.log(`${headDate}`)
+  //   var currentDate = await getDate.getUTCFromISO(headDate)
+  //   var config = await getConfig(context, 'weekly-digest.yml')
+  //   if (config == null) {
+  //     config = defaultConfig
+  //   }
+  //   if (currentDate.getDay() === config.publishDay) {
+  //     const { owner, repo } = context.repo()
+  //     // console.log(`Owner: ${owner}, Repo: ${repo}, Config: ${config.publishDay}`)
+  //     // console.log(`Head Date: ${headDate}, Tail Date: ${tailDate}, Current Day: ${currentDate.getDay()}`)
+  //     weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
+  //   }
+  // })
 }
