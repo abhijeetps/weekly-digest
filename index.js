5ef858b22ff9b9c87453aba61e2153e30ff54dcb
aps120797 <aps.120797@gmail.com>
2018-06-11 21:28:55 +0530

    removed comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const createWeeklyDigestLabel = require('./lib/markdown/createWeeklyDigestLabel'
 const createConfigYML = require('./lib/markdown/createConfigYML')
 const weeklyDigest = require('./lib/weeklyDigest')
 const getDate = require('./lib/markdown/getDate')
-// const defaultConfig = require('./lib/markdown/defaultConfig')
+const defaultConfig = require('./lib/markdown/defaultConfig')
 
 module.exports = (app) => {
   app.log('Weekly Digest app is ready to generate Weekly Reports!')
@@ -20,21 +20,21 @@ module.exports = (app) => {
     weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
   })
   createScheduler(app, {interval: 60 * 60 * 1000}) // 1 hour
-  // app.on('schedule.repository', async (context) => {
-  //   console.log(`App is running as per schedule.repository`)
-  //   const headDate = await getDate.headDate()
-  //   const tailDate = await getDate.tailDate()
-  //   // console.log(`${headDate}`)
-  //   var currentDate = await getDate.getUTCFromISO(headDate)
-  //   var config = await getConfig(context, 'weekly-digest.yml')
-  //   if (config == null) {
-  //     config = defaultConfig
-  //   }
-  //   if (currentDate.getDay() === config.publishDay) {
-  //     const { owner, repo } = context.repo()
-  //     // console.log(`Owner: ${owner}, Repo: ${repo}, Config: ${config.publishDay}`)
-  //     // console.log(`Head Date: ${headDate}, Tail Date: ${tailDate}, Current Day: ${currentDate.getDay()}`)
-  //     weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
-  //   }
-  // })
+  app.on('schedule.repository', async (context) => {
+    console.log(`App is running as per schedule.repository`)
+    const headDate = await getDate.headDate()
+    const tailDate = await getDate.tailDate()
+    // console.log(`${headDate}`)
+    var currentDate = await getDate.getUTCFromISO(headDate)
+    var config = await getConfig(context, 'weekly-digest.yml')
+    if (config == null) {
+      config = defaultConfig
+    }
+    if (currentDate.getDay() === config.publishDay) {
+      const { owner, repo } = context.repo()
+      // console.log(`Owner: ${owner}, Repo: ${repo}, Config: ${config.publishDay}`)
+      // console.log(`Head Date: ${headDate}, Tail Date: ${tailDate}, Current Day: ${currentDate.getDay()}`)
+      weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
+    }
+  })
 }
