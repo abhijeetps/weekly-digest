24c1a8652c1fa0a25001819963f9c7b128804415
aps120797 <aps.120797@gmail.com>
2018-06-11 20:40:53 +0530

    Added date constraint.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const getConfig = require('probot-config')
 const createWeeklyDigestLabel = require('./lib/markdown/createWeeklyDigestLabel')
 const createConfigYML = require('./lib/markdown/createConfigYML')
 const weeklyDigest = require('./lib/weeklyDigest')
+const getDate = require('./lib/markdown/getDate')
+const defaultConfig = require('./lib/markdown/defaultConfig')
 
 module.exports = (app) => {
   app.log('Weekly Digest app is ready to generate Weekly Reports!')
@@ -11,12 +13,26 @@ module.exports = (app) => {
     const [owner, repo] = await context.payload.repositories[0].full_name.split('/')
     createWeeklyDigestLabel(context, {owner, repo})
     createConfigYML(context, {owner, repo})
+    const headDate = await getDate.headDate()
+    const tailDate = await getDate.tailDate()
     const config = await getConfig(context, 'weekly-digest.yml')
-    weeklyDigest(context, {owner, repo}, config)
+    weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
   })
   app.on('schedule.repository', async (context) => {
-    const { owner, repo } = context.repo()
-    const config = await getConfig(context, 'weekly-digest.yml')
-    weeklyDigest(context, {owner, repo}, config)
+    console.log(`App is running as per schedule.repository`)
+    const headDate = await getDate.headDate()
+    const tailDate = await getDate.tailDate()
+    // console.log(`${headDate}`)
+    var currentDate = await getDate.getUTCFromISO(headDate)
+    var config = await getConfig(context, 'weekly-digest.yml')
+    if (config == null) {
+      config = defaultConfig
+    }
+    if (currentDate.getDay() === config.publishDay) {
+      const { owner, repo } = context.repo()
+      // console.log(`Owner: ${owner}, Repo: ${repo}, Config: ${config.publishDay}`)
+      // console.log(`Head Date: ${headDate}, Tail Date: ${tailDate}, Current Day: ${currentDate.getDay()}`)
+      weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
+    }
   })
 }
