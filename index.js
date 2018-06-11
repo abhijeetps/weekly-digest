4a1fa482b3c654d7a52b70c449fd621481647450
aps120797 <aps.120797@gmail.com>
2018-06-11 12:32:37 +0530

    Added probot-config and getConfig method

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const createScheduler = require('probot-scheduler')
+const getConfig = require('probot-config')
 const createWeeklyDigestLabel = require('./lib/markdown/createWeeklyDigestLabel')
 const createConfigYML = require('./lib/markdown/createConfigYML')
 const weeklyDigest = require('./lib/weeklyDigest')
 
-module.exports = (robot) => {
-  robot.log('Weekly Digest app is ready to generate Weekly Reports!')
-  createScheduler(robot, {interval: 60 * 60 * 1000}) // 1 hour
-  robot.on('installation.created', async (context) => {
+module.exports = (app) => {
+  app.log('Weekly Digest app is ready to generate Weekly Reports!')
+  createScheduler(app, {interval: 60 * 1000}) // 1 hour
+  app.on('installation.created', async (context) => {
     const [owner, repo] = await context.payload.repositories[0].full_name.split('/')
     createWeeklyDigestLabel(context, {owner, repo})
     createConfigYML(context, {owner, repo})
-    weeklyDigest(context, {owner, repo})
+    const config = await getConfig(context, 'weekly-digest.yml')
+    weeklyDigest(context, {owner, repo}, config)
   })
-  robot.on('schedule.repository', async (context) => {
+  app.on('schedule.repository', async (context) => {
     const { owner, repo } = context.repo()
-    weeklyDigest(context, {owner, repo})
+    const config = await getConfig(context, 'weekly-digest.yml')
+    weeklyDigest(context, {owner, repo}, config)
   })
 }
