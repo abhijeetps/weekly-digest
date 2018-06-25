e630acde009049c5e12741f1b3b5f5b7bc0e1e87
aps120797 <aps.120797@gmail.com>
2018-06-25 22:19:53 +0530

    Testing semantic-release

--- test/markdown/getLongMonth.test.js
+++ test/markdown/getLongMonth.test.js
@@ -1,4 +1,10 @@
+// var getLongDate = require('./../../src/markdown/getLongMonth')
+
 test('that we can run tests', () => {
   // your real tests go here
   expect(1 + 2 + 3).toBe(6)
 })
+
+// test('that passing 0 returns january', () => {
+//   expect(getLongDate(0)).toBe('January')
+// })
