6ae480a31bec789227d959684848b13b5e16a0f3
aps120797 <aps.120797@gmail.com>
2018-06-27 11:49:30 +0530

    Added emptyIssue payload

---
+++ test/payload/emptyIssue.js
@@ -0,0 +1,29 @@
+module.exports = {
+  data: [],
+  meta:
+  {
+    'access-control-allow-origin': '*',
+    'access-control-expose-headers': 'ETag, Link, Retry-After, X-GitHub-OTP, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval',
+    'cache-control': 'private, max-age=60, s-maxage=60',
+    connection: 'close',
+    'content-length': '2',
+    'content-security-policy': 'default-src \'none\'',
+    'content-type': 'application/json; charset=utf-8',
+    date: 'Wed, 27 Jun 2018 05:25:42 GMT',
+    etag: '"38513674d8297f47a6ed96543226b7bb"',
+    'referrer-policy': 'origin-when-cross-origin, strict-origin-when-cross-origin',
+    server: 'GitHub.com',
+    status: '200 OK',
+    'strict-transport-security': 'max-age=31536000; includeSubdomains; preload',
+    vary: 'Accept, Authorization, Cookie, X-GitHub-OTP',
+    'x-content-type-options': 'nosniff',
+    'x-frame-options': 'deny',
+    'x-github-media-type': 'github.squirrel-girl-preview',
+    'x-github-request-id': '4568:0F5E:3A4EDCC:79713F4:5B331FD6',
+    'x-ratelimit-limit': '5000',
+    'x-ratelimit-remaining': '4992',
+    'x-ratelimit-reset': '1530080490',
+    'x-runtime-rack': '0.036793',
+    'x-xss-protection': '1; mode=block'
+  }
+}
