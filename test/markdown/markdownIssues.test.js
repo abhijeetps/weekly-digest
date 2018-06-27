9a4e0e378ef64c71f47cfebb2f12c2c9423af499
aps120797 <aps.120797@gmail.com>
2018-06-27 11:53:31 +0530

    Added tests if there are no issues

--- test/markdown/markdownIssues.test.js
+++ test/markdown/markdownIssues.test.js
@@ -2,3 +2,13 @@ test('that we can run tests', () => {
   // your real tests go here
   expect(1 + 2 + 3).toBe(6)
 })
+
+const markdownIssues = require('./../../src/markdown/markdownIssues')
+
+const emptyIssue = require('./../payload/emptyIssue')
+const payload = jest.fn().mockReturnValue(emptyIssue)
+
+test('that checks return string if array is empty', () => {
+  expect(markdownIssues(payload)).toContain('# ISSUES')
+  expect(markdownIssues(payload)).toContain('This week, no issues have been created or closed.')
+})
