cf2af6a04dbf475cbce3066f64dab5a4196d8b1a
aps120797 <aps.120797@gmail.com>
2018-06-27 12:53:12 +0530

    Added tests to check return string there's no PR

--- test/markdown/markdownPullRequests.test.js
+++ test/markdown/markdownPullRequests.test.js
@@ -2,3 +2,13 @@ test('that we can run tests', () => {
   // your real tests go here
   expect(1 + 2 + 3).toBe(6)
 })
+
+const markdownPullRequests = require('./../../src/markdown/markdownPullRequests')
+
+const emptyPullRequests = require('./../payload/emptyPullRequests')
+const payload = jest.fn().mockReturnValue(emptyPullRequests)
+
+test('that checks return string if array is empty', () => {
+  expect(markdownPullRequests(payload)).toContain('# PULL REQUESTS')
+  expect(markdownPullRequests(payload)).toContain('This week, no pull requests has been proposed by the users.')
+})
