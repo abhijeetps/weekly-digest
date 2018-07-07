467642f22c2403c6dc6c5199c64e3f0e84055264
aps120797 <aps.120797@gmail.com>
2018-07-07 22:34:18 +0530

    Added issues payload.

---
+++ test/payload/issues.js
@@ -0,0 +1,80 @@
+module.exports = {
+  data: [
+    {
+      html_url: 'https://github.com/aps120797/playground/issues/4',
+      number: 4,
+      title: 'Weekly Digest test4',
+      user: {
+        type: 'USER',
+        login: 'aps120797',
+        html_url: 'https://github.com/aps120797'
+      },
+      state: 'open',
+      comments: 2,
+      reactions: {
+        '+1': 0,
+        laugh: 0,
+        hooray: 0,
+        heart: 0,
+        total_count: 0
+      }
+    },
+    {
+      html_url: 'https://github.com/aps120797/playground/issues/3',
+      number: 3,
+      title: 'Weekly Digest test3',
+      user: {
+        type: 'USER',
+        login: 'aps120797',
+        html_url: 'https://github.com/aps120797'
+      },
+      state: 'closed',
+      comments: 3,
+      reactions: {
+        '+1': 0,
+        laugh: 1,
+        hooray: 0,
+        heart: 1,
+        total_count: 2
+      }
+    },
+    {
+      html_url: 'https://github.com/aps120797/playground/issues/2',
+      number: 2,
+      title: 'Weekly Digest test2',
+      user: {
+        type: 'USER',
+        login: 'aps120797',
+        html_url: 'https://github.com/aps120797'
+      },
+      state: 'open',
+      comments: 4,
+      reactions: {
+        '+1': 1,
+        laugh: 0,
+        hooray: 1,
+        heart: 0,
+        total_count: 2
+      }
+    },
+    {
+      html_url: 'https://github.com/aps120797/playground/issues/1',
+      number: 1,
+      title: 'Weekly Digest test1',
+      user: {
+        type: 'USER',
+        login: 'aps120797',
+        html_url: 'https://github.com/aps120797'
+      },
+      state: 'closed',
+      comments: 1,
+      reactions: {
+        '+1': 1,
+        laugh: 1,
+        hooray: 1,
+        heart: 1,
+        total_count: 4
+      }
+    }
+  ]
+}
