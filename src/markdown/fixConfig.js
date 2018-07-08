081cac11db13c936a1cfaa0cd5c3439b3aa9319c
aps120797 <aps.120797@gmail.com>
2018-07-08 21:36:25 +0530

    Added fixConfig.js

---
+++ src/markdown/fixConfig.js
@@ -0,0 +1,24 @@
+module.exports = (config) => {
+  if (!config.hasOwnProperty('publishDay')) {
+    config.publishDay = 0
+  }
+  if (!config.hasOwnProperty('canPublishIssues')) {
+    config.canPublishIssues = true
+  }
+  if (!config.hasOwnProperty('canPublishPullRequests')) {
+    config.canPublishPullRequests = true
+  }
+  if (!config.hasOwnProperty('canPublishContributors')) {
+    config.canPublishContributors = true
+  }
+  if (!config.hasOwnProperty('canPublishStargazers')) {
+    config.canPublishStargazers = true
+  }
+  if (!config.hasOwnProperty('canPublishCommits')) {
+    config.canPublishCommits = true
+  }
+  if (!config.hasOwnProperty('canPublishReleases')) {
+    config.canPublishReleases = true
+  }
+  return config
+}
