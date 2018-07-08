693351d07b685b0d0ce180a2e55bf57ddca048cf
aps120797 <aps.120797@gmail.com>
2018-07-08 10:53:27 +0530

    Added tests for stargazers

--- test/markdown/markdownStargazers.test.js
+++ test/markdown/markdownStargazers.test.js
@@ -1,14 +1,30 @@
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
 
 const markdownStargazers = require('./../../src/markdown/markdownStargazers')
 
-const emptyStargazers = require('./../payload/empty')
-const payload = jest.fn().mockReturnValue(emptyStargazers)
+const moment = require('moment')
+const MockDate = require('mockdate')
 
-test('that checks return string if array is empty', () => {
-  expect(markdownStargazers(payload)).toContain('# STARGAZERS')
-  expect(markdownStargazers(payload)).toContain('This week, no user has starred this repository.')
+MockDate.set(moment('2018-03-24'))
+
+const emptyStargazers = require('./../payload/empty')
+const stargazers = require('./../payload/stargazers')
+const stargazer = require('./../payload/stargazer')
+
+test('that checks return string if there are no stargazers', () => {
+  let tailDate = moment().subtract(7, 'days').format()
+  expect(markdownStargazers(emptyStargazers, tailDate)).toContain('# STARGAZERS')
+  expect(markdownStargazers(emptyStargazers, tailDate)).toContain('This week, no user has starred this repository.')
+})
+
+test('that checks return string if there are some stargazers', () => {
+  let tailDate = moment().subtract(7, 'days').format()
+  expect(markdownStargazers(stargazers, tailDate)).toContain('# STARGAZERS')
+  expect(markdownStargazers(stargazers, tailDate)).toContain('This week, 2 users have starred this repository.')
+  expect(markdownStargazers(stargazers, tailDate)).toContain('They are [gr2m](https://github.com/gr2m), and [wilhelmklopp](https://github.com/wilhelmklopp).')
+})
+
+test('that checks return string if there is one stargazers', () => {
+  let tailDate = moment().subtract(7, 'days').format()
+  expect(markdownStargazers(stargazer, tailDate)).toContain('# STARGAZERS')
+  expect(markdownStargazers(stargazer, tailDate)).toContain('This week, [aps120797](https://github.com/aps120797) has starred the repository.')
 })
---
+++ test/payload/stargazers.js
@@ -0,0 +1,25 @@
+module.exports = {
+  data: [
+    {
+      user: {
+        login: 'gr2m',
+        html_url: 'https://github.com/gr2m'
+      },
+      starred_at: '2018-03-23T00:00:00Z'
+    },
+    {
+      user: {
+        login: 'wilhelmklopp',
+        html_url: 'https://github.com/wilhelmklopp'
+      },
+      starred_at: '2018-03-21T00:00:00Z'
+    },
+    {
+      user: {
+        login: 'aps120797',
+        html_url: 'https://github.com/aps120797'
+      },
+      starred_at: '2018-03-15T00:00:00Z'
+    }
+  ]
+}
