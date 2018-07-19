efb579d80636f7209d93859a2b68c4f690ecdfc6
aps120797 <aps.120797@gmail.com>
2018-07-19 12:29:35 +0530

    Used moment methods :zap: and added tests :white_check_mark:

--- test/markdown/getDate.test.js
+++ test/markdown/getDate.test.js
@@ -1,15 +1,21 @@
 const getDate = require('./../../src/markdown/getDate')
+
 const moment = require('moment')
 const MockDate = require('mockdate')
-
-MockDate.set(moment('2018-03-24'))
+MockDate.set(moment.utc('2018-04-24'))
 
 test('that headDate method returns current date', () => {
-  let date = moment().format()
+  let date = moment.utc().format()
   expect(getDate.headDate()).toEqual(date)
 })
 
 test('that tailDate method returns current date - 7 days', () => {
-  let date = moment().subtract(7, 'days').format()
+  let date = moment.utc().subtract(7, 'days').format()
   expect(getDate.tailDate()).toEqual(date)
+})
+
+test('that checks if getDayBeforeDate method returns day before today', () => {
+  let date = moment.utc().subtract(1, 'days').format()
+  let testDate = moment.utc('2018-04-24')
+  expect(getDate.getDayBeforeDate(testDate)).toEqual(date)
 })
