69446bdcadac8c9be67b3c703916ef527046504b
abhijeetps <abhijeetps@outlook.com>
2018-08-04 12:29:03 +0530

    Added intallation and installation_repositories events

--- index.js
+++ index.js
@@ -12,42 +12,53 @@ const fixConfig = require('./src/markdown/fixConfig')
 const checkDuplicates = require('./src/markdown/checkDuplicates')
 
 // 12 hours
-const interval = 60 * 1000
+const interval = 12 * 60 * 60 * 1000
+const events = ['installation.created', 'installation_repositories']
 
 module.exports = (app) => {
   app.log('Weekly Digest app is running successfully.')
 
-  app.on('installation.created', async (context) => {
+  app.on(events, async (context) => {
     app.log('App has been successfully installed.')
+    app.log('App is running as per ' + context.event)
     app.log('Local Time: ' + moment().format())
-    const [owner, repo] = await context.payload.repositories[0].full_name.split('/')
-    console.log(`Repository: ${owner}/${repo}`)
-    await postCreateLabel(context, {owner, repo, name: 'weekly-digest', color: '9C27B0', description: ''})
     const headDate = getDate.headDate()
     const tailDate = getDate.tailDate()
     const config = defaultConfig
-    weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
+    let repoList = []
+    if (context.event === 'installation' && context.payload.action === 'created') {
+      repoList = context.payload.repositories
+      console.log(repoList)
+    } else if (context.event === 'installation_repositories') {
+      repoList = context.payload.repositories_added
+    }
+    repoList.forEach(async (item) => {
+      const [owner, repo] = item.full_name.split('/')
+      app.log(`Repository: ${owner}/${repo}`)
+      await postCreateLabel(context, {owner, repo, name: 'weekly-digest', color: '9C27B0', description: ''})
+      await weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
+    })
   })
 
   createScheduler(app, {interval: interval})
   app.on('schedule.repository', async (context) => {
-    console.log(`App is running as per schedule.repository`)
+    app.log(`App is running as per schedule.repository`)
     app.log('Local Time: ' + moment().format())
     const { owner, repo } = context.repo()
-    console.log(`Repository: ${owner}/${repo}`)
+    app.log(`Repository: ${owner}/${repo}`)
     const headDate = getDate.headDate()
     const tailDate = getDate.tailDate()
     let { hasDuplicates, url } = await checkDuplicates(context, {owner, repo, headDate})
     if (hasDuplicates) {
       console.log(`Weekly Digest for this week has already been published for ${owner}/${repo}`)
       console.log(`URL: ` + url)
-    } else {
-      let config = await getConfig(context, 'weekly-digest.yml')
-      config = fixConfig(config)
-      console.log(`Publish Day: ${getNumDayFromLongDay(config.publishDay)}, Today: ${moment().day()}`)
-      if (moment().day() === getNumDayFromLongDay(config.publishDay)) {
-        weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
-      }
+      return
+    }
+    let config = await getConfig(context, 'weekly-digest.yml')
+    config = fixConfig(config)
+    console.log(`Publish Day: ${getNumDayFromLongDay(config.publishDay)}, Today: ${moment.utc().day()}`)
+    if (moment.utc().day() === getNumDayFromLongDay(config.publishDay)) {
+      await weeklyDigest(context, {owner, repo, headDate, tailDate}, config)
     }
   })
 }
---
+++ index.test.js
@@ -0,0 +1,37 @@
+const { Application } = require('probot')
+// Requiring our app implementation
+const myProbotApp = require('..')
+
+const installationCreatedPayload = require('./payload/issues.opened.json')
+
+describe('My Probot app', () => {
+  let app, github
+
+  beforeEach(() => {
+    app = new Application()
+    // Initialize the app based on the code from index.js
+    app.load(myProbotApp)
+    // This is an easy way to mock out the GitHub API
+    github = {
+      issues: {
+        createLabel: jest.fn().mockReturnValue(Promise.resolve({}))
+      }
+    }
+    // Passes the mocked out GitHub API into out app instance
+    app.auth = () => Promise.resolve(github)
+  })
+
+  test.skip('creates a comment when an issue is opened', async () => {
+    // Simulates delivery of an issues.opened webhook
+    await app.receive({
+      event: 'installation.created',
+      payload: installationCreatedPayload
+    })
+
+    // This test passes if the code in your index.js file calls `context.github.issues.createComment`
+    expect(github.issues.createLabel).toHaveBeenCalled()
+  })
+})
+
+// For more information about testing with Jest see:
+// https://facebook.github.io/jest/
