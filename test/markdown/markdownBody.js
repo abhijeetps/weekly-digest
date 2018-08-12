2fc60575e8237cabc4d12896f123b48ef69219c8
abhijeetps <abhijeetps@outlook.com>
2018-08-12 15:52:31 +0530

    Added tests for markdownBody :white_check_mark:

--- test/markdown/markdownBody.js
+++
@@ -1,3 +0,0 @@
-test('that 1 + 2 is 3', () => {
-  expect(1 + 2).toBe(3)
-})
---
+++ test/markdown/markdownBody.test.js
@@ -0,0 +1,59 @@
+
+const markdownBody = require('./../../src/markdown/markdownBody')
+const config = require('./../../src/markdown/defaultConfig')
+const falseConfig = require('./../payload/falseConfig')
+const moment = require('moment')
+const MockDate = require('mockdate')
+MockDate.set(moment.utc('2018-04-24'))
+let headDate = moment.utc().format()
+let tailDate = moment.utc().subtract(7, 'days').format()
+
+test('that markdownBody works', () => {
+  let owner = 'abhijeetps'
+  let repo = 'playground'
+  let context = {
+    github: {
+      paginate: jest.fn(),
+      issues: {
+        getForRepo: jest.fn(),
+        create: jest.fn()
+      },
+      pullRequests: {
+        getAll: jest.fn()
+      },
+      repos: {
+        getCommits: jest.fn(),
+        getReleases: jest.fn()
+      },
+      activity: {
+        getStargazersForRepo: jest.fn()
+      }
+    }
+  }
+  expect(markdownBody(context, {owner, repo, headDate, tailDate}, config)).toBeDefined()
+})
+
+test('that markdownBody works when the configs defined are all false', () => {
+  let owner = 'abhijeetps'
+  let repo = 'playground'
+  let context = {
+    github: {
+      paginate: jest.fn(),
+      issues: {
+        getForRepo: jest.fn(),
+        create: jest.fn()
+      },
+      pullRequests: {
+        getAll: jest.fn()
+      },
+      repos: {
+        getCommits: jest.fn(),
+        getReleases: jest.fn()
+      },
+      activity: {
+        getStargazersForRepo: jest.fn()
+      }
+    }
+  }
+  expect(markdownBody(context, {owner, repo, headDate, tailDate}, config)).toBeDefined()
+})
