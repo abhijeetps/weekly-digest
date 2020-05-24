58b7b8009fdde3eca9e6ee9abddbf916a5c2be77
abhijeetps <abhijeetps@outlook.com>
2020-05-24 15:52:37 +0530

    :fire: Remove test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   verbose: true,
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
   testURL: 'http://localhost/',
   collectCoverageFrom: [
     'src/**/*.js',
